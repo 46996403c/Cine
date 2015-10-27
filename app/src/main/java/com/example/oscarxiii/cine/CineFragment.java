2807c18966f79970eaa2d3b1a9f34fccf73f644b
oscar <46996403c@iespoblenou.org>
2015-10-27 19:45:40 +0100

    Agregamos menu de items

--- app/src/main/java/com/example/oscarxiii/cine/CineFragment.java
+++ app/src/main/java/com/example/oscarxiii/cine/CineFragment.java
@@ -3,6 +3,8 @@ package com.example.oscarxiii.cine;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -15,7 +17,6 @@ import java.util.Arrays;
  * A placeholder fragment containing a simple view.
  */
 public class CineFragment extends Fragment {
-
     private ArrayList<String> items;
     private ArrayAdapter<String> adaptador;
 
@@ -23,11 +24,16 @@ public class CineFragment extends Fragment {
     }
 
     @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_refresh, menu);
+    }
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
