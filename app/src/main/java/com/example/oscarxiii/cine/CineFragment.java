438c960f90791d539342bdc7b2f304fcbcdb0a6e
oscar <46996403c@iespoblenou.org>
2015-10-27 19:18:29 +0100

    Creado el array de la lista de peliculas

--- app/src/main/java/com/example/oscarxiii/cine/CineFragment.java
+++ app/src/main/java/com/example/oscarxiii/cine/CineFragment.java
@@ -1,26 +1,39 @@
 package com.example.oscarxiii.cine;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class CineFragment extends Fragment {
 
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adaptador;
+
     public CineFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        //return inflater.inflate(R.layout.fragment_cine, container, false);
         View rootView = inflater.inflate(R.layout.fragment_cine, container,false);
-        //inflater.inflate(R.layout.fragment_cine, container, false);
 
-        View lvPelis = rootView.findViewById(R.id.listaPelis);
+        ListView listaPeli = (ListView) rootView.findViewById(R.id.listaPelis);
+        items = new ArrayList<>();
+        adaptador = new ArrayAdapter<>(
+                getContext(),
+                R.layout.lista_peliculas,
+                R.id.tvTituloPelis,
+                items
+                );
+        listaPeli.setAdapter(adaptador);
         return rootView;
     }
 }
