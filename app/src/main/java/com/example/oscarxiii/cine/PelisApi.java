186491d00b704df29de862ebe272512dc1be342d
oscar <46996403c@iespoblenou.org>
2015-11-03 18:14:29 +0100

    inserto apiKey de mejores peliculas

--- app/src/main/java/com/example/oscarxiii/cine/PelisApi.java
+++ app/src/main/java/com/example/oscarxiii/cine/PelisApi.java
@@ -13,8 +13,6 @@ import retrofit.Response;
 import retrofit.Retrofit;
 import retrofit.http.GET;
 
-
-
 public class PelisApi {
     //https://api.themoviedb.org/3/movie/550?api_key=c82d8a6c928270dc97f66357f99880a5
     final String BASE_URL = "https://api.themoviedb.org/3/";
@@ -36,13 +34,37 @@ public class PelisApi {
             public void onResponse(Response<PelisPiojo> respuesta, Retrofit retrofit) {
                 if (respuesta.isSuccess()) {
                     PelisPiojo apiData = respuesta.body();
-                   // System.out.println("RESULTADO OK" + apiData.getMovies().toString());
+                    // System.out.println("RESULTADO OK" + apiData.getMovies().toString());
                     adaptador.clear();
                     /*
                     for (Movie peli : apiData.getMovies()) {
                         adaptador.add(peli.getTitle());
                     }
                     */
+                    for (Result peli : apiData.getResults()) {
+                        adaptador.add(peli.getTitle());
+                    }
+                } else {
+                    System.out.println("RESULTADO FAIL: " + respuesta.errorBody().toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                System.out.println("RESULTADO FAIL --> onFailure");
+            }
+        });
+    }
+
+    public void getMillorsPelicules(final ArrayAdapter adaptador) {
+        Call<PelisPiojo> llamada = servicio.getMillorsPelicules();
+        llamada.enqueue(new Callback<PelisPiojo>() {
+            @Override
+            public void onResponse(Response<PelisPiojo> respuesta, Retrofit retrofit) {
+                if (respuesta.isSuccess()) {
+                    PelisPiojo apiData = respuesta.body();
+                    // System.out.println("RESULTADO OK" + apiData.getMovies().toString());
+                    adaptador.clear();
                     for (Result peli : apiData.getResults()){
                         adaptador.add(peli.getTitle());
                     }
@@ -62,12 +84,7 @@ public class PelisApi {
 interface cineInterface {
     @GET("discover/movie?sort_by=popularity.desc&api_key=c82d8a6c928270dc97f66357f99880a5")
     Call<PelisPiojo> getPeliculesMesVistes();//(
-       // @Query("apikey") String apiKey
-//);
-    /*
-    @GET("lists/movies/upcoming.json")
-    Call<ApiData> getProximesEstrenes(
-        @Query("country") String pais,
-        @Query("apikey") String apiKey);
-        */
+
+    @GET("movie/top_rated?api_key=c82d8a6c928270dc97f66357f99880a5")
+    Call<PelisPiojo> getMillorsPelicules();
 }
