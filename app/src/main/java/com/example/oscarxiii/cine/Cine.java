c949ece2f8a69aa34b2ff59ffe590306b6ab0c85
oscar <46996403c@iespoblenou.org>
2015-11-12 15:25:40 +0100

    Quito los botones del mensaje que aparce en el layout abajo a la derecha

--- app/src/main/java/com/example/oscarxiii/cine/Cine.java
+++ app/src/main/java/com/example/oscarxiii/cine/Cine.java
@@ -2,13 +2,10 @@ package com.example.oscarxiii.cine;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class Cine extends AppCompatActivity {
 
@@ -19,6 +16,7 @@ public class Cine extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +24,7 @@ public class Cine extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
         });
+        */
 
     }
 
